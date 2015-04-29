7fd041483cd953869db87f7a76ea8cac84731117
Thunder Powell <ojp3@cornell.edu>
2015-04-29 13:42:43 -0400

    kl

--- index.php
+++ index.php
@@ -166,9 +166,9 @@ function csvJSON(csv){
           //go throgh the parsed and get everyones imdb score
           for( var x = 0; x<10 ;x++){
          //   http://www.omdbapi.com/?t=7+Faces+of+Dr.+Lao&y=1964&plot=short&r=json
-              var url = "http://www.omdbapi.com/?"
+              var url2 = "http://www.omdbapi.com/?"
               var title = parsed[x].movie, year = parsed[x].year;
-              url  = url + "t="+title +"&y="+ year +"&r=json&callback=?"
+              url2  = url2 + "t="+title +"&y="+ year +"&r=json&callback=?"
                   $.ajax({
                     type: "GET",
                     url: url,
